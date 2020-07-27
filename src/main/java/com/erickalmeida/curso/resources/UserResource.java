f7030331d619c3a3755c30883b83d2dc65149f99
Erick Almeida <erickprofissional@gmail.com>
2020-07-27 01:21:03 -0300

    classe UserServices e Pesquisa por Id na classe UserResource criado

--- src/main/java/com/erickalmeida/curso/resources/UserResource.java
+++ src/main/java/com/erickalmeida/curso/resources/UserResource.java
@@ -1,24 +1,41 @@
 package com.erickalmeida.curso.resources;
 
+import com.erickalmeida.curso.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erickalmeida.curso.entities.User;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/users") 
 
 
 public class UserResource {
-	
+
+	@Autowired  private UserService services;
+
+	public UserResource(UserService services) {
+		this.services = services;
+	}
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		
-		User u = new User(1L, "Erick", "erickprofissional@gmail.com","9999-9999","12345");
-		return ResponseEntity.ok().body(u);
-		
+		List<User> list = services.findAll();
+
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+
+		User obj = services.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
